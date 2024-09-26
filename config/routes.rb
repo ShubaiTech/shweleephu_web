41c23421b65f43e056eefeeb0b1d952e7c095029
Kyaw Lin <kyawlin@talenox.com>
2024-09-26 10:48:57 +0800

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/privacy'
-  get 'pages/terms'
+  get 'privacy', to: 'pages#privacy'
+  get 'terms', to: 'pages#terms'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
